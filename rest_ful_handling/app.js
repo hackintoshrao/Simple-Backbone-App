98be4fcfd87b7dc7098a8eac28f9d0d7598fc916
Karthic Rao <kartronics85@gmail.com>
2013-10-16 03:53:21 +0530

    The structure of the project is changed to handle the backbone requests at the backend , next on=bjective is to convert the project to locomotive

--- rest_ful_handling/app.js
+++ rest_ful_handling/app.js
@@ -28,7 +28,10 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/users', function(req,res){
+	console.log("backbone Request Accepted");
+	
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
