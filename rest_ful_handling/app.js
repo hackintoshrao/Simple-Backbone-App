f3c9d22416ea9aafe7843957576680113e2b75f9
Karthic Rao <kartronics85@gmail.com>
2013-10-20 12:11:14 +0530

    table bootstrap attribute applied to get a bordered well rendered table , responsiveness is added to the table , more json data from the server is sent to render on the page

--- rest_ful_handling/app.js
+++ rest_ful_handling/app.js
@@ -30,7 +30,22 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', function(req,res){
   console.log("backbone Request Accepted");
-  var obj = {test:1};
+  var obj = [
+              {"name":'Karthic Rao',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Manas',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Naveen',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Santhosh',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Ravi',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Kishore',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Chethan',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Mayur',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Jake',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Sahana',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Ishan',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Chaudary',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Ramya',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'},
+              {"name":'Cyrus',"age":20,"college":'FSMK',"branch":'Computers',"passion":'Keyboards,Metal,Coding'}
+            ]  
   res.writeHead(200,{'Content-Type':'application/json'});
   res.write(JSON.stringify(obj));
   res.end();
